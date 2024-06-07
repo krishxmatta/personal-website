24fe8060a47a72feed67815e70584d93e325a8ae
Krish Matta <self@krishmatta.net>
2024-06-07 00:01:45 -0700

    Use last modified

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "krishxmatta.github.io/personal-website/",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
